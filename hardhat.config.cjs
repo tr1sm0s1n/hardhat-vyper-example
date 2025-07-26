8f269c9721e3bba1e6eb809a3d2bb82daf54c357
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-07-26 22:40:38 +0530

    📌 | use vyper compiler '0.4.3'
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,8 +1,7 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-vyper");
+require('@nomicfoundation/hardhat-toolbox')
+require('@nomiclabs/hardhat-vyper')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-  vyper: "0.3.10",
-};
+  vyper: '0.4.3',
+}
