b93e638b4c04bc59477563001b23480625526cca
tr1sm0s1n <mobinm.mohanan@gmail.com>
2025-07-26 22:38:27 +0530

    🚚 | rename 'deploy.js' => 'Cert.deploy.js'
    Signed-off-by: tr1sm0s1n <mobinm.mohanan@gmail.com>

--- scripts/Cert.deploy.js
+++ scripts/Cert.deploy.js
@@ -4,23 +4,23 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-import hre from "hardhat";
-import { writeFile } from "fs";
+import hre from 'hardhat'
+import { writeFile } from 'fs'
 
-const cert = await hre.ethers.deployContract("Cert");
+const cert = await hre.ethers.deployContract('Cert')
 
-await cert.waitForDeployment();
+await cert.waitForDeployment()
 
 const details = {
   deployer: cert.deploymentTransaction().from,
   contract: await cert.getAddress(),
-};
+}
 
-console.log(`${details.deployer} deployed ${details.contract}`);
+console.log(`${details.deployer} deployed ${details.contract}`)
 
-writeFile("artifacts/details.json", JSON.stringify(details, null, 2), (err) => {
+writeFile('artifacts/details.json', JSON.stringify(details, null, 2), (err) => {
   if (err) {
-    return console.log(err);
+    return console.log(err)
   }
-  return console.log("Details are saved!!");
-});
+  return console.log('Details are saved!!')
+})
